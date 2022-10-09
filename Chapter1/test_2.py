20d81bfeb52f5d056ae8ab388c104a57348ee240
David Gugea <davidgugea@yahoo.com>
2022-10-09 15:33:23 +0200

    Finished Chapter 1

---
+++ Chapter1/test_2.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class SomeTestCase(unittest.TestCase):
+    def test_something(self):
+        # Arrange phase, nothing to prepare here.
+
+        # Act phase, call do_something
+        result = do_something()
+
+        # Assert phase, verify do_something did what we expect.
+        assert result == "did something"
